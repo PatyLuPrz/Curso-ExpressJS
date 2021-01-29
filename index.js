54127b8fffb06659da3863b3a232f6b429f69055
PatyLuPrz <1717110752@utectulancingo.edu.mx>
2021-01-28 20:31:42 -0600

    Routing: Manejo de URLs

--- index.js
+++ index.js
@@ -1,10 +1,50 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000,()=>{
-    console.log('Server on port 3000');
+app.use(express.json());
+
+app.listen(8080,()=>{
+    console.log('Server on port 8080');
+});
+
+app.all('/user',(req,res,next)=>{
+    console.log('Por aqui paso');
+    next();
 });
 
 app.get('/',(req,res)=>{
     res.send("Hola mundo!");
+});
+
+app.get('/about',(req,res)=>{
+    res.send("Get recibido!");
+});
+
+app.post('/post',(req,res)=>{
+    res.send("post recibido!");
+});
+
+app.put('/put',(req,res)=>{
+    res.send("put recibido!");
+});
+
+app.delete('/delete',(req,res)=>{
+    res.send("delete recibido!");
+});
+
+app.get('/user',(req,res)=>{
+    res.json({
+        "username":"Patricia",
+        "email":"may.patrics@gmail.com"
+    });
+});
+
+app.post('/user',(req,res)=>{
+    console.log(req.body);
+    res.send("Post user recibido!");
+});
+
+app.post('/user/:id',(req,res)=>{
+    console.log(req.params.id);
+    res.send("Post user id recibido!");
 });
